4ef5eb2a6090d872c271a845bb81066bcd6e4dd4
Jeshua Cienfuegos Estrada <156126427+JeshuaCien@users.noreply.github.com>
2025-05-19 20:40:14 -0600

    Controlador de Baile y Botónes
    Luego lo explico

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{   
+    [SerializeField]
+    private List<GameObject>_buttons;
+
+    [SerializeField]
+    private string _appearAnimationName;
+
+     [SerializeField]
+     private string _DisappearAnimationName;
+
+      [SerializeField]
+      private float _buttonsAppearDelay;
+    
+
+     public void ShowButtons()
+     {
+        StartCoroutine(ShowButtonWithDelay());
+     }
+
+     private IEnumerator ShowButtonWithDelay() 
+     {
+        foreach(GameObject button in _buttons)
+        {
+            button.GetComponent<Animator>().Play(_appearAnimationName);
+            yield return new WaitForSeconds(_buttonsAppearDelay);
+        }
+     }
+
+     public void HideButtons()
+     {
+        foreach(GameObject button in _buttons) {
+            button.GetComponent<Animator>().Play(_DisappearAnimationName);
+        }
+     }
+}
